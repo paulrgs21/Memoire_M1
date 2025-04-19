c6a1caed10aec694c4198e4532aaffe90f8befe4
Raphael <raphael.capranico@gmail.com>
2025-04-19 12:18:08 +0200

    new graph

--- Plot graphique by Raph.R
+++ Plot graphique by Raph.R
@@ -1,4 +1,7 @@
 rm(list = ls())
+library(tidyverse)
+library(survival)
+library(survminer)
 library(TraMineR)
 
 #### Data management 
@@ -13,29 +16,44 @@ data <- read.table("C:/Users/rapha/Desktop/Mémoire/donnees.txt",
 
 sequence_cols <- paste0("c", 1:94)
 seq_data <- seqdef(data[, sequence_cols], states = as.character(1:9),
-                   labels = c("CDI", "CDD", "Alt", "Public", "Intérim", "Chômage", "Inactif", "Service", "Études"))
+                   labels = c("Permanent employment", "Fixed-term employment", "work-study programs", "Youth employment", "Agency work", "Unemployed", "Inactive", "Military service", "Studies"))
 
 # Distribution par temps
-seqdplot(seq_data, with.legend = TRUE, legend.position = "bottom", legend.prop = 0.22)
+seqdplot(seq_data, 
+         with.legend = FALSE, # Désactive la légende intégrée
+         cex.legend = 0.8)
+write.csv(legend_table, "legend_table.csv", row.names = FALSE)
+
+# Version alternative avec knitr pour un rendu plus propre (si besoin)
+# knitr::kable(legend_table, caption = "Légende des états")
+seqdplot(seq_data, with.legend = TRUE, legend.position = "bottom", legend.prop = 0.23)
 
 
 #Comparer l'entropie avant/après une réforme politique (ex : loi sur les CDD).
 # Globalement les sujets ont 23 ans en 1998 et on a eu la réformes 
 
 # Avec l'entropi on verifie la predictibilité des changements d'état
-# Exemple on voit bien que en fin de carrière l'etat est beaucoup plus prévisible exemple
-start <- which(colnames(data) == "c8")
-end <- which(colnames(data) == "c18")
-Debut_carriere <- data[, start:end]
 
-start <- which(colnames(data) == "c12")
-end <- which(colnames(data) == "c29")
-Fin_carriere <- data[, start:end]
+base_H <- data[data$Q1==1,]
+start <- which(colnames(base_H) == "c1")
+end <- which(colnames(base_H) == "c29")
+base_H <- base_H[, start:end]
 
+base_F <- data[data$Q1==2,]
+start <- which(colnames(base_F) == "c1")
+end <- which(colnames(base_F) == "c29")
+base_F <- base_F[, start:end]
 
-seq_data <- seqdef(Debut_carriere)
+seq_data <- seqdef(base_H)
 entropies <- seqient(seq_data)
-hist(entropies, main = "Distribution des entropies", xlab = "Entropie")
-seq_data <- seqdef(Fin_carriere)
+hist(entropies, main = "Men Entropy Distribution", xlab = "Entropie")
+seq_data <- seqdef(base_F)
 entropies <- seqient(seq_data)
-hist(entropies, main = "Distribution des entropies", xlab = "Entropie")
+hist(entropies, main = "Women Entropy Distribution", xlab = "Entropie")
+
+
+
+
+
+
+
